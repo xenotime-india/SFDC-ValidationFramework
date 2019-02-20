1c246e527da8c28c087e2f5ce30a62918a326953
xenotime-india <sandeepkhoj@gmail.com>
2019-02-20 23:39:13 +0530

    implemented required field validation with field pojo class

--- force-app/main/default/aura/CreateContactForm/CreateContactFormController.js
+++ force-app/main/default/aura/CreateContactForm/CreateContactFormController.js
@@ -2,17 +2,29 @@
   clickCreate: function(component, event, helper) {
     const newContact = component.get('v.newContact');
     const action = component.get('c.createContact');
-    action.setParams({ newContactObj: newContact });
+    action.setParams({ payload: JSON.stringify(newContact) });
     action.setCallback(this, function(response) {
       const results = response.getReturnValue();
       const state = response.getState();
       if (state === 'SUCCESS') {
-        // Process server success response
-        helper.showToast({
-          type: 'success',
-          title: 'Success!',
-          message: 'The record has been created successfully.'
-        });
+        console.log(results);
+        if (results) {
+          component.find('contactform').forEach(inputCmp => {
+            if (results[inputCmp.get('v.name')]) {
+              inputCmp.setCustomValidity(results[inputCmp.get('v.name')]);
+            } else {
+              inputCmp.setCustomValidity('');
+            }
+            inputCmp.reportValidity();
+          });
+        } else {
+          // Process server success response
+          helper.showToast({
+            type: 'success',
+            title: 'Success!',
+            message: 'The record has been created successfully.'
+          });
+        }
       } else if (state === 'ERROR') {
         // Process error returned by server
         const errors = response.getError();
