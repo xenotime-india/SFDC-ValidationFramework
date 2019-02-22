dc3cbe3f11e7b570e1ffc79cce29b584d53db935
xenotime-india <sandeepkhoj@gmail.com>
2019-02-22 19:31:39 +0530

    added logic for group validation

--- force-app/main/default/aura/CreateContactForm/CreateContactFormController.js
+++ force-app/main/default/aura/CreateContactForm/CreateContactFormController.js
@@ -1,52 +1,57 @@
 ({
-  clickCreate: function(component, event, helper) {
-    const newContact = component.get('v.newContact');
-    const action = component.get('c.createContact');
-    action.setParams({ payload: JSON.stringify(newContact) });
-    action.setCallback(this, function(response) {
-      const results = response.getReturnValue();
-      const state = response.getState();
-      if (state === 'SUCCESS') {
-        console.log(results);
-        if (results) {
-          component.find('contactform').forEach(inputCmp => {
-            if (results[inputCmp.get('v.name')]) {
-              inputCmp.setCustomValidity(results[inputCmp.get('v.name')]);
-            } else {
-              inputCmp.setCustomValidity('');
-            }
-            inputCmp.reportValidity();
-          });
-        } else {
-          // Process server success response
-          helper.showToast({
-            type: 'success',
-            title: 'Success!',
-            message: 'The record has been created successfully.'
-          });
-        }
-      } else if (state === 'ERROR') {
-        // Process error returned by server
-        const errors = response.getError();
-        let message = 'Unknown error'; // Default error message
-        // Retrieve the error message sent by the server
-        if (errors && Array.isArray(errors) && errors.length > 0) {
-          message = errors.reduce(function(accumulator, error) {
-            if (error.message) {
-              return accumulator + error.message;
-            }
-            if (error.pageErrors && error.pageErrors.length > 0) {
-              return accumulator + error.pageErrors[0].message;
-            }
-          }, '');
-        }
-        helper.showToast({
-          type: 'error',
-          title: 'Error!',
-          message: message
-        });
-      }
-    });
-    $A.enqueueAction(action);
-  }
+	doInit: function(component, event, helper) {
+		// Retrieve contacts during component initialization
+		helper.loadForm(component);
+	},
+	clickCreate: function(component, event, helper) {
+		const newContact = component.get('v.newContact');
+		const action = component.get('c.createContact');
+		action.setParams({ payload: JSON.stringify(newContact) });
+		action.setCallback(this, function(response) {
+			const results = response.getReturnValue();
+			const state = response.getState();
+			if (state === 'SUCCESS') {
+				if (results) {
+					component.find('contactform').forEach(inputCmp => {
+						if (results[inputCmp.get('v.name')]) {
+							inputCmp.setCustomValidity(results[inputCmp.get('v.name')]);
+						} else if (results[inputCmp.get('v.group')]) {
+							inputCmp.setCustomValidity(results[inputCmp.get('v.group')]);
+						} else {
+							inputCmp.setCustomValidity('');
+						}
+						inputCmp.reportValidity();
+					});
+				} else {
+					// Process server success response
+					helper.showToast({
+						type: 'success',
+						title: 'Success!',
+						message: 'The record has been created successfully.'
+					});
+				}
+			} else if (state === 'ERROR') {
+				// Process error returned by server
+				const errors = response.getError();
+				let message = 'Unknown error'; // Default error message
+				// Retrieve the error message sent by the server
+				if (errors && Array.isArray(errors) && errors.length > 0) {
+					message = errors.reduce(function(accumulator, error) {
+						if (error.message) {
+							return accumulator + error.message;
+						}
+						if (error.pageErrors && error.pageErrors.length > 0) {
+							return accumulator + error.pageErrors[0].message;
+						}
+					}, '');
+				}
+				helper.showToast({
+					type: 'error',
+					title: 'Error!',
+					message: message
+				});
+			}
+		});
+		$A.enqueueAction(action);
+	}
 });
--- force-app/main/default/aura/CreateContactForm/CreateContactFormHelper.js
+++ force-app/main/default/aura/CreateContactForm/CreateContactFormHelper.js
@@ -1,4 +1,31 @@
 ({
+  loadForm: function(cmp) {
+    // Load all contact data
+    var action = cmp.get('c.init');
+    action.setCallback(this, function(response) {
+      var state = response.getState();
+      if (state === 'SUCCESS') {
+        console.log(response.getReturnValue());
+        cmp.set('v.newContact', response.getReturnValue());
+      }
+
+      // Display toast message to indicate load status
+      var toastEvent = $A.get('e.force:showToast');
+      if (state === 'SUCCESS') {
+        toastEvent.setParams({
+          title: 'Success!',
+          message: ' New contact form have been loaded successfully.'
+        });
+      } else {
+        toastEvent.setParams({
+          title: 'Error!',
+          message: ' Something has gone wrong.'
+        });
+      }
+      toastEvent.fire();
+    });
+    $A.enqueueAction(action);
+  },
   showToast: function(message) {
     const toastEvent = $A.get('e.force:showToast');
     toastEvent.setParams(message);
